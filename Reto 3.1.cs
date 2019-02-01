21c173c8f1766387b319c37ebeb4b7a12e5059e0
Santiago350 <47002827+Santiago350@users.noreply.github.com>
2019-01-31 19:42:45 -0500

    Add files via upload

---
+++ Reto 3.1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SMMLV = 828116;
+            double Salario = double.Parse(Console.ReadLine());
+            double A = 2 * SMMLV;
+            double B = 4 * SMMLV;
+
+            if (Salario < (2 * SMMLV))
+            {
+                Console.WriteLine("Se aplica la tarifa A");
+            }
+            if ( A <= Salario && Salario <= B & Salario != B)
+            {
+                Console.WriteLine("Se aplica la tarifa B");
+            }
+            else if (Salario >= B)
+            {
+                Console.WriteLine("Se aplica la tarifa C");
+            }
+        }
+    }
+}
---
+++ Reto 3.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SMMLV = 828116;
+            double Salario = double.Parse(Console.ReadLine());
+            double A = 2 * SMMLV;
+            double B = 5 * SMMLV;
+            double CuotaA = 3200; // Menos de 2 SMMLV
+            double CuotaB = 12700; // Entre 2 o mas SMMLV hasta 5 SMMLV 
+            double CuotaC = 33500; // 5 o mas SMMLV
+
+            if (Salario < A)
+            {
+                Console.WriteLine("Se aplica la tarifa A");
+                Console.WriteLine("La cuota de moderadora es: " + CuotaA);
+            }
+            if (A <= Salario && Salario <= B & Salario != B)
+            {
+                Console.WriteLine("Se aplica la tarifa B");
+                Console.WriteLine("La cuota de moderadora es: " + CuotaB);
+            }
+            else if ( Salario >= B)
+            {
+                Console.WriteLine("Se aplica la tarifa C");
+                Console.WriteLine("La cuota de moderadora es: " + CuotaC);
+            } 
+        }
+    }
+}
