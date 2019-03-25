6f7c44f70a7a6f23dc22e7764789bdf5a2f1003b
Santiago350 <47002827+Santiago350@users.noreply.github.com>
2019-03-25 12:58:37 -0500

    Add files via upload

--- Parcial.cs
+++ Parcial.cs
@@ -7,6 +7,7 @@ class Parcial {
 
     string[] names;
     double[] data;
+    public static int codigo = 0; // 0 Primer simulacro. 1 Segundo simulacro
 
     public Parcial(string[] _names, double[] _data) {
         data = new double[_data.Length];
@@ -17,29 +18,34 @@ class Parcial {
 
 
     public int PrimerPunto() {
-        string[] nombres = new string[names.Length];
-        names.CopyTo(nombres, 0);
-        double[] notas = new double[data.Length];
-        data.CopyTo(notas, 0);
-        int salida = 0 ;
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
 
+        int salida = 0 ;
         //--------------------------------------------
         //- Abajo de esta línea va su código ---------
       
 
+        for (int i = 0; i < notas.Length; i++)
+        {
+           
+
+            if(notas[i] <= 3.0)
+            {
+                salida++;                
+            }
+        }
+
 
         //- Arriba de esta línea va su código --------
-
         return salida;
     }
 
     public string[] SegundoPunto() {
-        string[] nombres = new string[names.Length];
-        names.CopyTo(nombres, 0);
-        double[] notas = new double[data.Length];
-        data.CopyTo(notas, 0);
-        string[] salida = new string[5];
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
 
+        string[] salida = new string[5];
         //--------------------------------------------
         //- Abajo de esta línea va su código ---------
         double tem = 0;
@@ -59,37 +65,34 @@ class Parcial {
                     nombres[i] = nombres[i + 1];
                     nombres[i + 1] = tem2;
                 }
-
+                
             }
 
         }
-
-        for (int i =0; i < salida.Length; i++)
+        for (int i = 0; i < notas.Length; i += 1)
         {
-                salida[i] = nombres[i];
-                Console.WriteLine("Nombre " + nombres[i]);
-                Console.WriteLine("Nota " + notas[i]);
-            
+
+            Console.WriteLine("Nombre " + nombres[i]);
+            Console.WriteLine("Puntaje " +notas[i]);
         }
-        Console.WriteLine("\n " );
+        for (int i = 0; i < 5; i++)
+        {
+            salida[i] = nombres[i];
+           
+        }
 
 
         //- Arriba de esta línea va su código --------
-
         return salida;
     }
 
     public string[] TercerPunto() {
-        string[] nombres = new string[names.Length];
-        names.CopyTo(nombres, 0);
-        double[] notas = new double[data.Length];
-        data.CopyTo(notas, 0);
-        string[] salida = new string[0];
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
 
+        string[] salida = new string[0];
         //--------------------------------------------
         //- Abajo de esta línea va su código ---------
-       
-
         double tem = 0;
         string tem2 = "";
         for (int j = 0; j < notas.Length - 1; j += 1)
@@ -111,49 +114,76 @@ class Parcial {
             }
 
         }
-        int contadorN = 0;
-        for (int i =0; i < notas.Length; i++)
-        {
-            if (notas[i] < 3.0)
-            {
-                contadorN += 1;
-            }
-        }
-
-        salida = new string[contadorN];
-
-        for (int j = 0; j < contadorN; j++)
+        for (int i = 0; i < notas.Length; i += 1)
         {
 
-            for (int i = j; i < notas.Length; i++)
+            Console.WriteLine("Nombre " + nombres[i]);
+            Console.WriteLine("Puntaje " + notas[i]);
+        }
+        int cantidad = 0;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] <= 3.0)
             {
-                if ( notas[i] <= 3.0)
-                {
-                    salida[j] = nombres[i];
-                    break;
-                }
+                cantidad++;               
             }
         }
+        salida = new string [cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            salida[i]= nombres[i];
+        }
+
+
         //- Arriba de esta línea va su código --------
-
         return salida;
     }
 
     public string[] CuartoPunto() {
-        string[] nombres = new string[names.Length];
-        names.CopyTo(nombres, 0);
-        double[] notas = new double[data.Length];
-        data.CopyTo(notas, 0);
-        string[] salida = new string[0];
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
 
+        string[] salida = new string[0];
         //--------------------------------------------
         //- Abajo de esta línea va su código ---------
+        char letra = 'A';
+        int tamaño = 0;
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            string nombre = nombres[i];
+            
+            for (int j = 0; j < nombres[i].Length; j++)
+            {
+                char letraanalizada = nombre[j];
+                if (letra == letraanalizada)
+                {
+                    tamaño++;
+                    break;
+                }
+            }
+        }
+        salida = new string[tamaño];
 
+        int contador = 0;
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            string nombre = nombres[i];
+            for (int j = 0; j < nombres[i].Length; j++)
+            {
+                char letraanalizada = nombre[j];
+                if (letra == letraanalizada)
+                {
+                    salida[contador] = nombres[i];
+                    contador++;
+                    break;
+                }
+            }
+        }
 
 
         //- Arriba de esta línea va su código --------
-
         return salida;
     }
 }
+
 
